7f17de2d65d9b3926a4df3cdefbdd01c98739972
Yaoyuan <s.yaoyuan.zhang@gmail.com>
2016-07-22 13:19:59 +0800

    learn to predict

---
+++ ml5.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+from sklearn.svm import SVC
+iris=datasets.load_iris()
+clf=SVC()
+clf.fit(iris.data,iris.target)
+
+list(clf.predict(iris.data[:3]))
+
+clf.fit(iris.data, iris.target_names[iris.target])
+
+list(clf.predict(iris.data[:3]))
+
